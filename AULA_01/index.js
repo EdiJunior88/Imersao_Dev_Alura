e4d7f490c6c51db2da11a0a32f3aeb9c28f21392
EdiJunior88 <er.moura88@yahoo.com.br>
2022-09-05 22:20:56 -0300

    Atualização da AULA 01 - Adicionando logos SVG - Estilizando a página (CSS) - Criação do código (HTML) - Adicionando comentários - Adicionando função toFixed

---
+++ AULA_01/index.js
@@ -0,0 +1,23 @@
+var nome = "Edivaldo Junior";
+var notaDoPrimeiroBimestre = 9;
+var notaDoSegundoBimestre = 7;
+var notaDoTerceiroBimestre = 4;
+var notaDoQuartoBimestre = 2;
+
+var notaFinal =
+  (notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestre +
+    notaDoQuartoBimestre) /
+  4;
+
+var notaFixada = notaFinal.toFixed(1);
+
+console.log("Bem vindo " + nome);
+
+console.log(notaFixada);
+
+// isso é um comentário
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
